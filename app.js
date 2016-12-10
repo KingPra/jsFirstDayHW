7315751cbb25f30fd75e893fc8e0d5d0735fc1f0
KingPra <kingi.pra@gmail.com>
2016-12-09 23:02:48 -0500

    working on palindromes

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ console.log('js homework');
 Create an array that contains a bunch of numbers representing the heights of various mountains. Write some code that identifies the height of the tallest of the available mountains and prints it out.
 
  */
+console.log('Tallest Mountain')
 
 
 let mtn = [1200, 12205, 4000, 7600];
 let tallest = 0;
-for (i = 0; i < mtn.length; i++) {
+for (let i = 0; i < mtn.length; i++) {
     if (tallest < mtn[i]) {
         tallest = mtn[i];
     
@@ -24,12 +25,14 @@ console.log(tallest);
 Create a variable called word that contains a hangman word, and create a second variable called letter that indicates the letter that the player guessed. Print out "Yeah, the letter x exists y times in my word" if the letter exists (but substitute the actual letter and the number of times). If the letter doesn't exist, print out "Nope, that letter doesn't exist in my word."
 */
 
-let word = 'his';
-letter = 't'; 
-count = 0;
+console.log('Hangman Lite')
 
-for (i = 0; i < word.length; i++) {
-    if (letter === word [i]) {
+let word = 'his';
+let letter = 't'; 
+let count = 0;
+
+for (let i = 0; i < word.length; i++) {
+    if (letter === word[i]) {
         count++;
     }
 } 
@@ -56,16 +59,78 @@ let birthRate = 0.1;
 let numberOfWeeks = 5;
 ... then you're trying to figure out how many hares will exist after five weeks if we start off with 200 and increase their population by 10% each week.
 
+*/
+
+console.log('Cherokee Hare')
+
+
+let startingPopulation = 200;
+let birthRate = 0.1;
+let numberOfWeeks = 5;
+for (let i = 0; i < numberOfWeeks; i++) {
+    startingPopulation = startingPopulation + (startingPopulation * birthRate);
+}
+
+console.log('There will be '+Math.floor(startingPopulation)+' Cherokee Hares after '+numberOfWeeks+' weeks');
+
+
+
+
+/*
+
 Change machine
 
 Imagine we have a very versatile change machine. Create a variable called amount stores a quantity of dollars, and write some code that produces an array that indicates the number of 1, 5, 10, and 20 bills that the change machine should return. You want the change machine to return the smallest number of bills possible.
 
 After your code runs you should have a new array of the format [twenties, tens, fives, ones]. For example, if amount = 47, you should print an array after your code runs that looks like [2, 0, 1, 2] (two twenties, 0 tens, 1 five, 2 ones).
 
+*/
+console.log('Change Machine');
+
+
+
+let amount = 234;
+let change = [];
+
+change [0] = Math.floor(amount / 20);
+amount = amount % 20;
+
+change [1] = Math.floor(amount / 10);
+amount = amount % 10;
+
+change [2] = Math.floor(amount / 5);
+amount = amount % 5;
+
+change [3] = Math.floor(amount);
+
+
+console.log(change);
+
+
+
+/*
 Finding palindromes
 
 Create a variable called word that stores a word we want to test as a palindrome. Write some code that prints out "true" if word is a palindrome, or "false" if its not.
+*/
 
+console.log('Finding Palindromes');
+
+
+let words = 'abcd';
+let back = word.length - 1;
+
+for (let i = 0; i < words.length; i++) {
+    if (word[i] === word[back - i]) {
+   
+} 
+
+
+
+
+
+
+/** 
 Hard mode
 
 Create a variable called count that keeps track of the length of the desired array. Write some code that generates the first count numbers in the Fibonacci sequence.
